1ea8e77b069f82ed4cbb3df31aa8c52ec2a20087
Joshua Morgan <JoshuaReubenMorgan@gmail.com>
2022-06-25 23:03:07 +0100

    Finishing this off

--- Index.js
+++ Index.js
@@ -1,22 +1,78 @@
 const form = document.getElementById("form");
 const toDoInput = document.getElementById("toDoInput");
-const todos = document.getElementById("todos");
+const todosUL = document.getElementById("todos");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if(todos){
+    todos.forEach(todo => {
+        addAndDeleteTodo(todo)
+    });
+}
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const todoText = toDoInput.value;
+    addAndDeleteTodo();
+});
+
+//adds and removes the todo items 
+function addAndDeleteTodo(todo) {
+    let todoText = toDoInput.value;
+
+    if(todo) {
+        todoText = todo.text;
+    }
 
     if (todoText) {
         const todoEl = document.createElement("li");
+
+        if(todo && todo.completed) {
+            todoEl.classList.add("completed");
+        }
+        
         todoEl.innerText = todoText;
 
-        todoEl.addEventListener('click', () => {
-            todoEl.classList.toggle('completed')
-        })
+        todoEl.addEventListener("click", () => {
+            todoEl.classList.toggle("completed")
 
-        todos.appendChild(todoEl)
+            //Putting update here allows the completed flag to update when an item is clicked
+            updateLS();
+        });
+
+        todoEl.addEventListener("contextmenu", (e) => {
+            e.preventDefault();
+
+            todoEl.remove();
+
+            updateLS();
+        });
+
+        todosUL.appendChild(todoEl)
 
         toDoInput.value = "";
+
+        updateLS();
     }
-})
+}
+
+//updates local storage
+function updateLS() {
+    const todosEl = document.querySelectorAll("li");
+
+    const todos = [];
+    
+    //Note Elements are the list items DOM element
+    //notes will be an array of the notes 
+    todosEl.forEach((todoEl) => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains
+            ("completed"),
+        });
+    });
+
+    //Posts the value of the text and the completed flag, currently stores all of the notes in the same JSON rather than per note 
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
